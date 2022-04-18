de854c538f42a716cb9a4e20dedddeedb89556fb
Eduardo <eduardobarbaro.ifc@gmail.com>
2022-04-18 08:41:46 -0300

    master

---
+++ site com vue/sitevue/src/App.vue
@@ -0,0 +1,14 @@
+<script>
+  import MenuLateral from "@/components/MenuLateral.vue"
+  export default {
+    components: { MenuLateral },
+  };
+</script>
+
+<template>
+  <MenuLteral />
+  <header class="header">Cabeçalho</header>
+  <main class="content">Principal</main>
+  <footer class="footer">Rodapé</footer>
+</template>
+<style></style>
---
+++ site com vue/sitevue/src/components/MenuLteral.vue
@@ -0,0 +1,14 @@
+<script>
+    export default {};
+</script>
+<template>
+    <aside class="sidenav">
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+        </ul>
+    </aside>
+</template>
